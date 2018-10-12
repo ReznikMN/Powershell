a09458761fd96c26bc9d6750f3b35715bc2a5014
Reznik Mikhail <34116251+ReznikMN@users.noreply.github.com>
2018-10-12 09:40:55 +0300

    RAM and RarusLog scripts added
    RAM and RarusLog scripts added

---
+++ RAM.ps1
@@ -0,0 +1,11 @@
+﻿#$strComputer = Read-Host "Enter Computer Name"
+$colSlots = Get-WmiObject -Class win32_PhysicalMemoryArray -namespace root\CIMV2 -computerName .
+$colRAM = Get-WmiObject -Class win32_PhysicalMemory -namespace root\CIMV2 -computerName .
+
+
+write-host "Общее количество слотов DIMM: " $colSlots.MemoryDevices
+Foreach ($objRAM In $colRAM) {
+     "Слот: " + $objRAM.DeviceLocator
+     "Объем памяти: " + ($objRAM.Capacity / 1GB) + " GB"
+}
+Read-Host
---
+++ RarusLog.ps1
@@ -0,0 +1,13 @@
+﻿Set-ExecutionPolicy Bypass -Force
+
+$DateOfStart = Read-Host "Введите дату и время начала поиска, в формате: ММ.ДД.ГГГГ мм:сс (10.03.2018 20:00)"
+
+Write-Host "Идёт обработка входа, подождите..."
+Get-EventLog -Log Security -After $DateOfStart  -EntryType SuccessAudit | where {$_.InstanceID -eq 4648 -and $_.Message -like "*1crarus*"} | Sort-Object -Descending | Format-Table @{Label="Время входа москалей"; Expression={$_.TimeGenerated}}
+
+Write-Host "Идёт обработка выхода, подождите..."
+Get-EventLog -Log Security -After $DateOfStart  -EntryType SuccessAudit | where {$_.InstanceID -eq 4634 -and $_.Message -like "*1crarus*"} | Sort-Object -Descending | Format-Table @{Label="Время выхода москалей"; Expression={$_.TimeGenerated}}
+
+
+Write-Host "Нажмите любую клавишу, чтобы завершить программу."
+$Host.UI.RawUI.ReadKey() > $Null
