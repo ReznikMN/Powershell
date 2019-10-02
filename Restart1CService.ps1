32c5421d6c5e2537a7b4f00db65c51d83541312b
Reznik Mikhail <34116251+ReznikMN@users.noreply.github.com>
2019-10-02 15:56:24 +0300

    Add files via upload

---
+++ Restart1CService.ps1
@@ -0,0 +1,18 @@
+﻿Set-ExecutionPolicy Bypass -Force
+
+$COMConnector=New-Object -ComObject "V83.Comconnector"
+$hostname="192.168.221.26" # ip сервера кластера
+$serv_agent=$COMConnector.ConnectAgent($hostname) # коннектимся агентом к кластеру
+$cluster=($serv_agent.GetClusters())[0] # получаем объект кластера
+$serv_agent.Authenticate($cluster,"","") # аутентифицируемся на кластере
+$baseinfo=$serv_agent.GetInfoBases($cluster) # получаем список баз на кластере
+$dropsesCOM=$serv_agent.GetInfoBaseSessions($cluster,$bases) #получаем список сеансов к базе
+foreach($ses in $dropsesCOM){$serv_agent.terminateSession($cluster,$ses)} # удаляем найденные сессии
+
+
+Get-Service -DisplayName '*1с*' | Stop-Service
+
+Start-Sleep -Seconds 360
+
+Get-Service -DisplayName '*1с*' | Start-Service
+
